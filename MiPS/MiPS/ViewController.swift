242cc630d65207d765e2bef015d69403d2452cb8
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2023-09-19 23:34:14 +0900

    Feat: create base folder structure

---
+++ MiPS/MiPS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiPS
+//
+//  Created by 남유성 on 2023/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
